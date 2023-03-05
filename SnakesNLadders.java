35d8073c0ba68592200e2464880ab91a030e0653
Naveen <naveengorakala814@gmail.com>
2023-03-05 11:19:01 -0800

    Add:Rolls the Dice

--- SnakesNLadders.java
+++ SnakesNLadders.java
@@ -1,8 +1,14 @@
 import java.util.*;
+import java.util.Random;
 public class SnakesNLadders
 {
    public static void main(String args[])
    {
       System.out.println("Snake and Ladder game Played with Single Player Start Position:0");
+      int Start=0;
+      Random rand = new Random();
+      int dicei = rand.nextInt(6);
+      Start += dicei;
+      System.out.println("dice rolled: "+Start);
    }
 }
