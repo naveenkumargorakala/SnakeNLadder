b1c955c6bf4011dc948db0baac636224a4547bf6
Naveen <naveengorakala814@gmail.com>
2023-03-05 10:51:25 -0800

    Add: Welcome message

---
+++ SnakesNLadders.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SnakesNLadders
+{
+   public static void main(String args[])
+   {
+      System.out.println("Snake and Ladder game Played with Single Player Start Position:0");
+   }
+}
